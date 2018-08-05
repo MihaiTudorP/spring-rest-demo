10d0e6bb2c7263de64a162709dc425e19c67c969
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2018-08-05 17:16:07 +0300

    Added Project Lombok reference and Student entity in the model

---
+++ spring-rest-demo/src/main/java/com/luv2code/springrestdemo/model/Student.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.luv2code.springrestdemo.model;
+
+import lombok.Data;
+
+/**
+ * @author Mihai-Tudor Popescu
+ *
+ */
+public @Data class Student {
+	private String firstName;
+	private String lastName;
+}
